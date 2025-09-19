126a532181b1fc56601918c8ca9379a31587c180
Mohamed Izeldeen <eng.mohamed.izeldeen@gmail.com>
2025-09-19 21:17:30 +0400

    status : right now we have folder for backend and folder for frontend

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Api\SuperAdmin;
-
-Route::get('/SuperAdmin-dashboard', [SuperAdmin::class, 'dashboard'])->name('api.dashboard');
-
-
